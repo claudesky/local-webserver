f6fb6a26b4a1430e704a41dd3b054cc419d471aa
Claude Kirke <claudekirke@gmail.com>
2020-12-28 20:41:18 +0800

    feat (init script): add requirement to run as root
    docker-compose and the docker socket require root access to use

--- init-cert.sh
+++ init-cert.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ "$EUID" -ne 0 ]]; then
+  echo "Error: Please run as root." >&2
+  exit 1
+fi
+
 if ! [ -x "$(command -v docker-compose)" ]; then
   echo 'Error: docker-compose is not installed.' >&2
   exit 1
